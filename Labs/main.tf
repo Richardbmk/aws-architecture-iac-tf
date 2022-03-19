e2ad49a15e0762507989bb18322d829ea09bc8d7
Richardbmk <rdobmk@gmail.com>
2022-03-19 17:04:28 +0100

    new folder for experiments

---
+++ Labs/main.tf
@@ -0,0 +1,14 @@
+# --- root/main.tf --- 
+
+module "networking" {
+  source = "./modules/networking/"
+
+  # region = "${var.region}"
+  environment             = var.environment
+  subnate_group_name      = var.subnate_group_name
+  vpc_cidr                = var.vpc_cidr
+  public_subnets_cidr     = var.public_subnets_cidr
+  private_subnets_cidr    = var.private_subnets_cidr
+  private_db_subnets_cidr = var.private_db_subnets_cidr
+  availability_zones      = var.availability_zones
+}
